0a89a3c71b45c4bbf8641f0a1c64a47d8ce963f6
Saranya Adhikary <118829809+saranyaadhikary@users.noreply.github.com>
2024-02-26 07:38:45 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "saranyaadhikary93@gmai.com"
+var accountPassword = "12345"  // dont use now ,,, scope issue
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2   not allowed in node js
+
+accountEmail = "sa@sa.com"
+console.log(accountEmail);
+console.table([accountEmail,accountId,accountPassword , accountCity, accountState]);
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,10 @@
+// type of datatypes
+
+"use strict";     // use newer version of javaScript ,  this is a kind of rule
+// alert("hello")
+// alert( 5+5)                //we are using node JS not browser
+
+
+let age = 19
+console.log(age);
+console.log(typeof age);
